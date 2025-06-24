62a9ed269ff5a0e01874d5419979be55b74a432e
Kentro <kentroiletisim@gmail.com>
2025-06-24 22:52:06 +0300

    Update avatar.js

--- src/Genel Üye/avatar.js
+++ src/Genel Üye/avatar.js
@@ -4,33 +4,34 @@ const { EmbedBuilder } = require('discord.js');
 module.exports = {
   name: 'avatar',
   description: 'Kullanıcının avatarını gösterir.',
-  async execute(message, args) {
+  
+  async execute(client, message, args) {
     let user;
 
-    // Eğer mesaj reply'si varsa reply edilen kullanıcıyı al
-    if (message.reference) {
+    // 1. Öncelik: mesaj bir yanıta yazıldıysa, yanıtlanan kişinin avatarı
+    if (message.reference?.messageId) {
       try {
         const repliedMessage = await message.channel.messages.fetch(message.reference.messageId);
         user = repliedMessage.author;
       } catch {
-        // Eğer fetch başarısızsa kullanıcıyı mesaj sahibi yaparız
-        user = message.author;
+        user = null;
       }
     }
-    // Eğer argüman varsa (etiket veya id)
-    else if (args[0]) {
-      // Öncelikle mention kontrolü
-      user = message.mentions.users.first();
 
-      // Eğer mention yoksa, ID den dene
+    // 2. Etiket veya ID ile kullanıcıyı al
+    if (!user && args[0]) {
+      user = message.mentions.users.first();
       if (!user) {
-        user = await message.client.users.fetch(args[0]).catch(() => null);
+        user = await client.users.fetch(args[0]).catch(() => null);
       }
 
-      if (!user) user = message.author; // Bulamazsa mesaj sahibi olsun
-    } 
-    else {
-      // Hiç argüman yoksa mesaj sahibini al
+      if (!user) {
+        return message.channel.send("Kullanıcı bulunamadı veya geçersiz bir ID girildi.");
+      }
+    }
+
+    // 3. Hiçbir şey yoksa komutu yazan kişi
+    if (!user) {
       user = message.author;
     }
 
@@ -41,10 +42,10 @@ module.exports = {
       .setImage(avatarURL)
       .setColor('#2f3136')
       .setFooter({
-        text: config.footer || message.client.user.username,
-        iconURL: message.client.user.displayAvatarURL()
+        text: config.footer || client.user.username,
+        iconURL: client.user.displayAvatarURL()
       });
 
-    message.reply({ embeds: [embed] });
+    message.channel.send({ embeds: [embed] });
   }
 };
