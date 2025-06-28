ecaa20b873506968ec0d184833c87bf4ee8c106b
Kentro <kentroiletisim@gmail.com>
2025-06-28 23:00:06 +0300

    Update unlock.js

--- src/Yetkili/unlock.js
+++ src/Yetkili/unlock.js
@@ -3,27 +3,29 @@ const client = global.client;
 
 module.exports = {
   name: "unlock",
-  usage: "unlock",
+  usage: "unlock [#kanal]",
   category: "moderasyon",
   aliases: ["kilitaç", "kanalkilitaç"],
   execute: async (client, message, args, beş_embed) => {
     if (!message.member.permissions.has(PermissionFlagsBits.ManageChannels)) {
-      return message.reply({ embeds: [beş_embed.setDescription("> **Kilit açmak için yetkin yok!**")] }).sil(5);
+      return message.reply({ embeds: [beş_embed.setDescription("> **Kiliti açmak için `Kanalları Yönet` yetkisi gerek!**")] });
     }
 
+    const kanal = message.mentions.channels.first() || message.channel;
     const everyoneRole = message.guild.roles.cache.find(r => r.name === "@everyone");
+
     if (!everyoneRole) {
-      return message.reply({ embeds: [beş_embed.setDescription("> **@everyone rolü bulunamadı!**")] }).sil(5);
+      return message.reply({ embeds: [beş_embed.setDescription("> **@everyone rolü bulunamadı!**")] });
     }
 
-    if (message.channel.permissionsFor(everyoneRole).has(PermissionFlagsBits.SendMessages)) {
-      return message.reply({ embeds: [beş_embed.setDescription("> **Kanal zaten kilitli değil!**")] }).sil(5);
+    if (kanal.permissionsFor(everyoneRole).has(PermissionFlagsBits.SendMessages)) {
+      return message.reply({ embeds: [beş_embed.setDescription(`> **${kanal} zaten açık!**`)] });
     }
 
-    await message.channel.permissionOverwrites.edit(everyoneRole.id, {
+    await kanal.permissionOverwrites.edit(everyoneRole.id, {
       SendMessages: true,
     });
 
-    message.reply({ embeds: [beş_embed.setDescription(`> **Kanal kilidi başarıyla açıldı! 🔓**`)] });
+    message.reply({ embeds: [beş_embed.setDescription(`> **${kanal} kilidi başarıyla açıldı! 🔓**`)] });
   },
 };
